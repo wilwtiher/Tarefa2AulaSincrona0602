32961c5a5e70f12d3f02a529b799e82c7eb4f2ee
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-10 10:38:43 -0300

    questao 5 da tarefa

--- pwm.c
+++ pwm.c
@@ -10,18 +10,23 @@ int main()
 
     gpio_set_function(PWM_MOTOR, GPIO_FUNC_PWM);   // Habilitar o pino GPIO como PWM
     uint slice = pwm_gpio_to_slice_num(PWM_MOTOR); // Obter o canal (slice) PWM da GPIO
-    pwm_set_clkdiv(slice, 125.0);                    // Definir o divisor de clock do PWM
-    pwm_set_wrap(slice, 19999);                  // Definir o valor de wrap para 50 Hz
-    pwm_set_gpio_level(PWM_MOTOR, 1500);          // Definir o ciclo de trabalho para 1,5 ms (90 graus)
+    pwm_set_clkdiv(slice, 125.0);                  // Definir o divisor de clock do PWM
+    pwm_set_wrap(slice, 19999);                    // Definir o valor de wrap para 50 Hz
+    pwm_set_gpio_level(PWM_MOTOR, 1500);           // Definir o ciclo de trabalho para 1,5 ms (90 graus)
     pwm_set_enabled(slice, true);                  // Habilitar o PWM no slice correspondente
 
     while (true)
     {
         pwm_set_gpio_level(PWM_MOTOR, 2400); // Definir o ciclo de trabalho para 2400 us (180 graus)
-        sleep_ms(5000);                        // Aguardar 5 segundos
-        pwm_set_gpio_level(PWM_MOTOR, 1470);  // Definir o ciclo de trabalho para 1470 us (90 graus)
-        sleep_ms(5000);                        // Aguardar 5 segundos
+        sleep_ms(5000);                      // Aguardar 5 segundos
+        pwm_set_gpio_level(PWM_MOTOR, 1470); // Definir o ciclo de trabalho para 1470 us (90 graus)
+        sleep_ms(5000);                      // Aguardar 5 segundos
         pwm_set_gpio_level(PWM_MOTOR, 500);  // Definir o ciclo de trabalho para 500 us (0 graus)
-        sleep_ms(5000);                        // Aguardar 5 segundos
+        sleep_ms(5000);                      // Aguardar 5 segundos
+        for (int i = 500; i <= 2400; i = i + 5)
+        {
+            pwm_set_gpio_level(PWM_MOTOR, 500); // Definir o ciclo de trabalho para i us (ate 180 graus)
+            sleep_ms(10);                       // Aguardar 10 milissegundos
+        }
     }
 }
