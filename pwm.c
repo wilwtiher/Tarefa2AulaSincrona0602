2bf83b48277ca884b3b4f71b883970e5e9482269
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-10 10:23:11 -0300

    questoes de 1 a 4 da tarefa.

--- pwm.c
+++ pwm.c
@@ -2,22 +2,26 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h" //biblioteca para controlar o hardware de PWM
 
-#define PWM_MOTOR 22 // pino do motor conectado a GPIO como PWM
+#define PWM_MOTOR 22 // pino do motor conectado a GPIO como PWM. para o LED, mudar o pino para 12.
 
 int main()
 {
     stdio_init_all();
 
-    gpio_set_function(PWM_MOTOR, GPIO_FUNC_PWM);   // habilitar o pino GPIO como PWM
-    uint slice = pwm_gpio_to_slice_num(PWM_MOTOR); // obter o canal (slice) PWM da GPIO
-    pwm_set_clkdiv(slice, 2.0);                    // define o divisor de clock do PWM
-    pwm_set_wrap(slice, 2000);                     // definir o valor de wrap – valor máximo do contador PWM
-    pwm_set_gpio_level(PWM_MOTOR, 1500);           // definir o ciclo de trabalho (duty cycle) do pwm
-    pwm_set_enabled(slice, true);                  // habilitar o pwm no slice correspondente
+    gpio_set_function(PWM_MOTOR, GPIO_FUNC_PWM);   // Habilitar o pino GPIO como PWM
+    uint slice = pwm_gpio_to_slice_num(PWM_MOTOR); // Obter o canal (slice) PWM da GPIO
+    pwm_set_clkdiv(slice, 2.0);                    // Definir o divisor de clock do PWM
+    pwm_set_wrap(slice, 1249999);                  // Definir o valor de wrap para 50 Hz
+    pwm_set_gpio_level(PWM_MOTOR, 93750);          // Definir o ciclo de trabalho para 1,5 ms (90 graus)
+    pwm_set_enabled(slice, true);                  // Habilitar o PWM no slice correspondente
 
     while (true)
     {
-        printf("slice:%d\n", slice);
-        sleep_ms(1000);
+        pwm_set_gpio_level(PWM_MOTOR, 150000); // Definir o ciclo de trabalho para 2400 µs (180 graus)
+        sleep_ms(5000);                   // Aguardar 5 segundos
+        pwm_set_gpio_level(PWM_MOTOR, 91875);   // Definir o ciclo de trabalho para 1470 µs (90 graus)
+        sleep_ms(5000);                    // Aguardar 5 segundos
+        pwm_set_gpio_level(PWM_MOTOR, 31250);   // Definir o ciclo de trabalho para 500 µs (0 graus)
+        sleep_ms(5000);                    // Aguardar 5 segundos
     }
 }
